98247beabae245d9955245ac493da0109c0c4ad1
dthaiyen <70678828+dthaiyen@users.noreply.github.com>
2020-10-09 11:23:27 +0700

    Add project files.

---
+++ Model/ItemGroupModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class ItemGroupModel
+    {
+        public int parent_item_group_id { get; set; }
+        public int madm { get; set; }
+        public string tendm { get; set; }
+        
+        public int chiso { get; set; }
+        public List<ItemGroupModel> children { get; set; }
+        public string type { get; set; }
+    }
+}
